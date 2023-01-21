ca77672db86c1e8b4a5d955efd77d407acda0305
Nikita Kamyshenko <nikita.kamyshenko@csssr.com>
2023-01-21 20:11:54 +0400

    Init

---
+++ src/test/kotlin/com/example/nosqlkotlin/NoSqlKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.nosqlkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NoSqlKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
